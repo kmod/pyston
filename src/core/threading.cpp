54cc8d08f7e667ddcf46e409a57e65d22e1481a8
Kevin Modzelewski <kevmod@gmail.com>
2014-11-18 20:33:55 -0800

    need this

--- src/core/threading.cpp
+++ src/core/threading.cpp
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include <pthread.h>
 
 #include "core/threading.h"
 
@@ -18,7 +19,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include <err.h>
-#include <pthread.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 

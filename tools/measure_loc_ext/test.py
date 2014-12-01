7979c9d5fa573617b016d1b47acc24789f2acc4c
Kevin Modzelewski <kevmod@gmail.com>
2014-11-30 19:49:37 -0800

    -m support

--- tools/measure_loc_ext/test.py
+++ tools/measure_loc_ext/test.py
@@ -3,9 +3,10 @@ import sys
 sys.path.append("build/lib.linux-x86_64-2.7")
 # sys.path.append("..")
 
-import measure_loc
-
+import cPickle
 import runpy
+
+import measure_loc
 
 """
 sys.settrace(measure_loc.trace)
@@ -31,7 +32,12 @@ measure_loc.dump()
 if __name__ == "__main__":
     old_sys_argv = sys.argv
     fn = sys.argv[1]
-    args = sys.argv[2:]
+
+    if fn == '-m':
+        module = sys.argv[2]
+        args = sys.argv[3:]
+    else:
+        args = sys.argv[2:]
     sys.argv = [sys.argv[0]] + args
 
     sys.settrace(measure_loc.trace)
@@ -40,12 +46,17 @@ if __name__ == "__main__":
     sys.path[0] = os.path.abspath(os.path.dirname(fn))
 
     # del sys.modules["__main__"] # do we need this?
-    runpy.run_path(fn, run_name="__main__")
+    if fn == '-m':
+        runpy.run_module(module, run_name="__main__")
+    else:
+        runpy.run_path(fn, run_name="__main__")
     sys.settrace(None)
 
     # measure_loc.dump()
 
     times = measure_loc.get_times().items()
+    with open("measure_loc.pkl", "w") as f:
+        cPickle.dump(times, f)
     times.sort(key=lambda (l, t): t, reverse=True)
 
     total = 0.0
@@ -63,8 +74,10 @@ if __name__ == "__main__":
         fn, lineno = l
         total_lines += 1
         sofar += t
-        print ("%s:%s" % (fn, lineno)).ljust(40), "%.4fs %4.1f%% % 3d %4.1f%%" % (t, t / total * 100, total_lines, sofar / total * 100.0)
+        if total_lines <= 20:
+            print ("%s:%s" % (fn, lineno)).ljust(40), "%.4fs %4.1f%% % 3d %4.1f%%" % (t, t / total * 100, total_lines, sofar / total * 100.0)
         if sofar >= total * FRACTION:
             break
+    print "(and %d more -- see mesaure_loc.pkl)" % (total_lines - 20)
 
     print "Picked %d lines out of %d to reach %.2f%%" % (total_lines, len(times), sofar / total * 100.0)

6901e40430793e7fd796c49f1fe18dd6517a67ba
Kevin Modzelewski <kmod@dropbox.com>
2016-07-29 22:13:27 -0700

    test file

---
+++ test/tests/sjit_test.py
@@ -0,0 +1,28 @@
+def g():
+    pass
+def f(n):
+    f;f;f;f;f;f;f;f;f;f
+    f;f;f;f;f;f;f;f;f;f
+    f;f;f;f;f;f;f;f;f;f
+    f;f;f;f;f;f;f;f;f;f
+    f;f;f;f;f;f;f;f;f;f
+    f;f;f;f;f;f;f;f;f;f
+    f;f;f;f;f;f;f;f;f;f
+    f;f;f;f;f;f;f;f;f;f
+    f;f;f;f;f;f;f;f;f;f
+    f;f;f;f;f;f;f;f;f;f
+    # return n * n
+def _f(n):
+    n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n
+    n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n
+    n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n
+    n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n
+    n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n
+    n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n
+    n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n
+    n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n
+    n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n
+    n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n; n*n;n*n
+
+for i in xrange(2000000):
+    f(i)
